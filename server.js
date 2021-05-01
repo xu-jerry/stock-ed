7255bcb9dcf536d4bae1fae45207b499f806fcb8
jason2020 <jtay20@punahou.edu>
2021-04-30 17:54:43 -1000

    Created react project with express server

---
+++ server.js
@@ -0,0 +1,14 @@
+const morgan = require("morgan");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/test",(req, res) => {
+    console.log("Testing post request on /test!");
+});
+
+app.listen(port);
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={() => testServer()}>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function testServer() {
+  console.log("post request");
+  axios.post(`http://localhost:3000/test`, {})
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    });
+}
+
+export default App;
