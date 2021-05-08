cb9f169c2be2ee5ce7711147536ec45e269addec
Jerry Xu <xuchujun2672@gmail.com>
2021-05-07 21:23:30 -0700

    Login button redirects to login page

--- src/navigation/nav.js
+++ src/navigation/nav.js
@@ -1,11 +1,4 @@
-import { useHistory } from "react-router-dom";
-function Home(props) {
-  const history = useHistory();
-
-  const handleClick = () => {
-    history.push("/Login");
-  }
-
+function nav(props) {
     return (
       <>
       <head>
@@ -14,8 +7,7 @@ function Home(props) {
       </head>
       <body>
         <h1>Stock<blue>Ed</blue></h1>
-        <p>This is the Home page.</p>
-        <div class="button" onClick={() => handleClick()}><button>Login</button></div>
+        <div class="button" onClick={() => functionthatgoestologin()}><button>Login</button></div>
         <div class="button"><button>Signup</button></div>
       </body>
       </>
