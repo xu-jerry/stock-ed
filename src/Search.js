73f561071aefbf1e7951fe314bc25aef8cc2408f
jason2020 <jtay20@punahou.edu>
2021-05-12 20:23:21 -1000

    Fixed scroll.js name not matching import in search.js

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import Scroll from "./searchcomponents/Scroll"
+import Scroll from "./searchcomponents/scroll"
 import './Search.css'
 
 const popularStocks = [
