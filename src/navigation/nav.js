09c2d152636fd5a60a07fa37fd2c061f4c6243a1
Jerry Xu <xuchujun2672@gmail.com>
2021-05-15 00:06:34 -0700

    Added login page, fixed nav bar

--- src/navigation/nav.js
+++ src/navigation/nav.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { useHistory } from "react-router-dom";
   
 const Navbar = () => {
-    const history = useHistory();
+  const history = useHistory();
 
-    const handleHomeClick = () => {
-        history.push("/Home");
-      }
+  const handleHomeClick = () => {
+    history.push("/Home");
+  }
 
   const handleLoginClick = () => {
     history.push("/Login");
   }
   const handleStockClick = () => {
-    history.push("/Stock");
+    history.push("/Stock/:id");
   }
   const handleLeaderboardClick = () => {
     history.push("/Leaderboard");
@@ -25,12 +25,11 @@ const Navbar = () => {
   }
 
   return (
-    <div className="container">
-        <h1>Stock<span className="blue">Ed</span></h1>
+    <div className="header">
+        <h1 class="title">Stock<span className="blue">Ed</span></h1>
         <div className="center">
           <button className="button" onClick={() => handleHomeClick()}>Home</button>
-          <button className="button" onClick={() => handleLoginClick()}>Login</button>
-          <button className="button" onClick={() => handleLoginClick()}>Signup</button>
+          <button className="button" onClick={() => handleLoginClick()}>Login/Signup</button>
           <button className="button" onClick={() => handleStockClick()}>Stock</button>
           <button className="button" onClick={() => handleLeaderboardClick()}>Leaderboard</button>
           <button className="button" onClick={() => handleAboutClick()}>About</button>
