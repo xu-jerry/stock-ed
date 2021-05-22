ab7d580cc74be02fa34412709dd63e8c895492d8
Jerry Xu <jerryxu4@gmail.com>
2021-05-21 22:03:58 -0700

    Updated README

--- src/About.js
+++ src/About.js
@@ -2,7 +2,7 @@ function About(props) {
     return (
       <div className="page">
         <h1>Our Mission</h1>
-        <p>[insert text here]</p>
+        <p>We know that stocks can be daunting to get into, so we wanted to create an environment that could teach students who wanted to get into stocks. StockEd provides a risk-free place to buy and sell real stocks in real time, but not with real money.</p>
         <h1>How does this work?</h1>
         <p>[more text here]</p>
         <h1>Tutorial</h1>
