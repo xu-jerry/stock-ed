46da8e631787c503367c8c9ea5c8dc538dbe2494
Anica Wang <anicawang@gmail.com>
2021-05-21 22:29:06 -0700

    About

--- src/About.js
+++ src/About.js
@@ -1,7 +1,8 @@
 function About(props) {
     return (
       <div className="page">
-        <p>This is the About page.</p>
+        <p>StockEd is a fun, interactive web app that helps users of all ages develop financial literacy by mimicking the fluctuations of the stock market and allowing users to simulate realistic experiences in the stock market without having to suffer the potential consequences!
+</p>
       </div>
     );
   }
