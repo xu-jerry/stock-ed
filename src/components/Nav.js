01178aa7c3210f1d30c431cd435eefa8f322d625
arpitjalan18 <jalanarpit12@gmail.com>
2021-05-21 21:24:20 -0700

    Fix search link. Take stock off navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -36,7 +36,7 @@ const Navbar = (props) => {
         <h1 className="title">Stock<span className="blue">Ed</span></h1>
         <div className="center">
           <button className="button" onClick={() => handleHomeClick()}>Home</button>
-          <button className="button" onClick={() => handleStockClick()}>Stock</button>
+          {/*<button className="button" onClick={() => handleStockClick()}>Stock</button>*/}
           <button className="button" onClick={() => handleLeaderboardClick()}>Leaderboard</button>
           <button className="button" onClick={() => handleAboutClick()}>About</button>
           <button className="button" onClick={() => handleSearchClick()}>Search</button>
