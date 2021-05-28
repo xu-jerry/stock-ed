04cbfb59d15418c42cad3f03a9890325bedc6c9c
jason2020 <jtay20@punahou.edu>
2021-05-27 23:18:21 -1000

    Fixed merge and bad issues..?

--- src/About.js
+++ src/About.js
@@ -5,16 +5,10 @@ function About(props) {
         <p>We know that stocks can be daunting to get into, so we wanted to create an environment that could teach students who wanted to get into stocks. StockEd is a fun, interactive web app that helps users of all ages develop financial literacy by mimicking the fluctuations of the stock market and allowing users to simulate realistic experiences in the stock market without having to suffer the potential financial consequences!</p>
         <div className="grayBackground">
         <h1>How does this work?</h1>
-<<<<<<< HEAD
+        </div>
         <p>StockEd is a fun, interactive web app that helps users of all ages develop financial literacy by mimicking the fluctuations of the stock market and allowing users to simulate realistic experiences in the stock market without having to suffer the potential consequences!</p>
         <h1>Tutorial</h1>
         <p>To start, create an account or login if you have an existing one already. The search bar allows users to look up and analyze the fluctuations and prices of specific stocks. To buy a stock, head to the portfolio page and enter all the necessary information. Users can also compete with each other under the leaderboard page to determine who earns more money in a certain time period.</p>
-=======
-        <p>[more text here]</p>
-        </div>
-        <h1>Tutorial</h1>
-        <p>[optional]</p>
->>>>>>> 7d5577e5be5080abf36b3e7a4cbbd40c11477048
       </div>
     );
   }
