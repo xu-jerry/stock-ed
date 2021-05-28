d8e6d3e827500a9812c8219a77ddff2f1a7b5e84
jason2020 <jtay20@punahou.edu>
2021-05-28 02:24:24 -1000

    Working trade stock logic, tested with simulated userData. Just need to sync with firebase and account creation

--- src/base.js
+++ src/base.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { doc, setDoc, getFirestore } from "firebase/firestore"; 
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,
   onAuthStateChanged, signOut} from "firebase/auth";
+import axios from "axios";
 
 initializeApp({
   apiKey: "AIzaSyBsv-j022oT9Zp7LKo_3YzyjmXZDdGh6Xk",
@@ -17,9 +18,7 @@ initializeApp({
 const db = getFirestore();
 const auth = getAuth();
 
-/* Make a new user with provided username and password 
- * TODO: Handle the error, (change this method to async and handle that in Login)
-*/
+// Make a new user with provided username and password 
 export async function createUser(username, password) {
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, username, password);
@@ -50,7 +49,7 @@ export async function signIn(username, password) {
   }
 }
 
-export function logOut(afterSignout) {
+export function logOut() {
   return new Promise((resolve, reject) => {
     signOut(auth).then(() => {
       resolve(true);
@@ -82,9 +81,93 @@ export function checkLoginStatus() {
  */
 
 export async function getUserStockData(uid) {
+  if (!uid) {
+    return null;
+  }
+}
 
+export async function setUserStockData(uid, userData) {
+  if (!uid) {
+    return false;
+  }
+
+  try {
+    // Sync user's data on the database with updated version
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
 
 export async function getLeaderboardData(uid) {
+  if (!uid) {
+    return null;
+  }
+}
 
+/* Trade stock attemps to sell/buy a certain amount
+ * of a stock specified by the ticker. If the user is 
+ * not able to buy/sell the specified amount, then return 
+ * false. Only valid tickers can reach the sell/buy menu
+*/
+
+export async function tradeStock(ticker, amount) {
+  const uid = checkLoginStatus();
+  // Tested using: userData = {cash: 100000.00, stocks: {AAPL: {amount: 2, currentValue: 250.56}}};
+  let userData = getUserStockData(await uid);
+  
+  const currentValue = amount * (await axios.get("/stock", {params: {symbol: ticker}})).data.price.regularMarketPrice;
+  /* Check that the user is logged in 
+   * Also verify that the amount is a non-zero amount
+   * (this should never happen)
+   */
+  if (!(await userData) || amount === 0) {
+    return false;
+  }
+
+  if (amount < 0) {
+    // Handle stock selling
+    if (userData.stocks.hasOwnProperty(ticker) && userData.stocks[ticker].amount >= (-1 * amount)) {
+      userData.cash -= currentValue;
+      userData.stocks[ticker].amount += amount;
+      userData.stocks[ticker].currentValue += currentValue;
+
+      // Round stock value to the nearest penny
+      userData.stocks[ticker].currentValue = parseFloat(userData.stocks[ticker].currentValue.toFixed(2));
+
+      // Remove stock if all shares are sold
+      if (userData.stocks[ticker].amount === 0) {
+        delete userData.stocks[ticker];
+      }
+    } else {
+      console.log("Not enough stocks");
+      // Not enough stocks
+      return false;
+    }
+  } else  {
+    // Handle stock purchasing
+    if (userData.cash >= currentValue) {
+      if (userData.stocks.hasOwnProperty(ticker)) {
+        userData.stocks[ticker].amount += amount;
+        userData.stocks[ticker].currentValue += currentValue;
+      } else {
+        userData.stocks[ticker] = {
+          amount: amount,
+          currentValue: currentValue
+        }
+      }
+      userData.cash -= currentValue;
+    } else {
+      // Not enough cash
+      return false;
+    }
+    // Round stock value to the nearest penny
+    userData.stocks[ticker].currentValue = parseFloat(userData.stocks[ticker].currentValue.toFixed(2));
+  }
+
+  // Round cash to the nearest penny
+  userData.cash = parseFloat(userData.cash.toFixed(2));
+
+  console.log(userData);
+  return await setUserStockData(uid, userData);
 }
