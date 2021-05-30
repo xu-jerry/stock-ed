5307849859b54de97a6e380145d59cfe0bcfddc6
Jason Tay <jtay20@g.ucla.edu>
2021-05-29 19:24:19 -1000

    Fixing tab spacing

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/stock", (req, res) => {
-	const data =  getStockData(req.query.symbol, function(stockData) {
-			res.json(stockData);
+	const data = getStockData(req.query.symbol, function(stockData) {
+		res.json(stockData);
 	});
 });
 
 app.get("/historical", (req, res) => {
-	const data =  getHistoricalData(req.query.symbol, req.query.fromDate, req.query.toDate,function(historicalData) {
-			res.json(historicalData);
+	const data = getHistoricalData(req.query.symbol, req.query.fromDate, req.query.toDate,function(historicalData) {
+		res.json(historicalData);
 	});
 });
 
@@ -29,11 +29,11 @@ app.get("/stocknews", async (req, res) => {
 	//     return [articleTitle, articleLink];
 	// });
 	let economistNews = await getLatestNews("https://www.economist.com/finance-and-economics", 5, ($, i) => {
-			let articleTitle = $(`main > div > div.layout-section-collection > div:nth-child(${i}) > div.teaser__text >
-					h2 > a.headline-link > span.teaser__headline`).text();
-			let articleLink = $(`main > div > div.layout-section-collection > div:nth-child(${i}) > div.teaser__text >
-					h2 > a.headline-link`).attr('href');
-			return [articleTitle, "https://www.economist.com/" + articleLink];
+		let articleTitle = $(`main > div > div.layout-section-collection > div:nth-child(${i}) > div.teaser__text >
+				h2 > a.headline-link > span.teaser__headline`).text();
+		let articleLink = $(`main > div > div.layout-section-collection > div:nth-child(${i}) > div.teaser__text >
+				h2 > a.headline-link`).attr('href');
+		return [articleTitle, "https://www.economist.com/" + articleLink];
 	});
 	res.send(JSON.stringify(economistNews));
 });
@@ -42,27 +42,28 @@ app.listen(port);
 
 function getStockData(symbol, callback) {
 	yahooFinance.quote({
-			symbol: symbol,
-			modules: [ 'price', 'summaryDetail']
+		symbol: symbol,
+		modules: [ 'price', 'summaryDetail']
 	}, (err, quotes) => {
-			if (err) {
-					callback(null);  
-					return;
-			}
-			callback(quotes); 
+		if (err) {
+			callback(null);  
+			return;
+		}
+		callback(quotes); 
 	});
 }
+
 function getHistoricalData(symbol, fromDate, toDate,callback){
 	yahooFinance.historical({
-			symbol: symbol,
-			from: fromDate,
-			to: toDate,    
+		symbol: symbol,
+		from: fromDate,
+		to: toDate,    
 	}, (err, quotes) => {
-			if (err) {
-					callback(null);  
-					return;
-			}
-			callback(quotes); 
+		if (err) {
+				callback(null);  
+				return;
+		}
+		callback(quotes); 
 	});
 }
 
